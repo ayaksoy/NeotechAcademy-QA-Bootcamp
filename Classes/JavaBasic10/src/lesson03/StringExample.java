37d92cb08ffdc810e0900233b40262853864b2d0
Ahmet <yasir.aksoy@outlook.com>
2025-09-14 19:20:32 +0300

    Starting to string

---
+++ Classes/JavaBasic10/src/lesson03/StringExample.java
@@ -0,0 +1,13 @@
+package lesson03;
+
+public class StringExample {
+    public static void main(String[] args) {
+
+        String str = "ahmet";
+        String str2 = "memed";
+        System.out.println(str);
+        str = str2;
+        System.out.println(str);
+
+    }
+}
