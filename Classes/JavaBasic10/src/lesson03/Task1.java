04ad6fd5320ad5c02d1cdc4436c32709e02c82b1
Ahmet <yasir.aksoy@outlook.com>
2025-09-14 19:57:45 +0300

    Task1 done

---
+++ Classes/JavaBasic10/src/lesson03/Task1.java
@@ -0,0 +1,15 @@
+package lesson03;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String name = "pelin";
+        String lasName = "idonnow";
+        byte grade = 80;
+        String city = "Izmir";
+        String state = "Turkiye";
+
+        System.out.println("My name is " + name + " and my lastname is " + lasName +
+                ". I live in city of " + city + ", state of " + state +
+                ". My grade is: " + grade);
+    }
+}
