7ebe56f2f6fe79aa9a8189ab99bdb09eae56ffdc
Ahmet <yasir.aksoy@outlook.com>
2025-09-14 23:25:10 +0300

    Write a java program that converts mile to km and km to miles.

---
+++ Homeworks/BasicHomeworks/src/Homework/Lesson3Homework2.java
@@ -0,0 +1,13 @@
+package Homework;
+
+public class Lesson3Homework2 {
+    public static void main(String[] args) {
+        double mile = 132;
+        double km = mile * 1.609344;
+
+        System.out.println(mile + " Mile = " + km + " Km");
+        km = 75;
+        mile = km / 1.609344;
+        System.out.println(km + " Km = " + mile + " Mile");
+    }
+}
