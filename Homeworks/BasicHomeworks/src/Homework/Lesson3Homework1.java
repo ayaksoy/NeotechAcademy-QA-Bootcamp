2a98e6f8a26046b3aecb70f92211c610d6f54ba1
Ahmet <yasir.aksoy@outlook.com>
2025-09-14 23:15:33 +0300

    Write a java program to display the area and perimeter of a circle that has a radius of 5. Use pi = 3.

---
+++ Homeworks/BasicHomeworks/src/Homework/Lesson3Homework1.java
@@ -0,0 +1,17 @@
+package Homework;
+
+public class Lesson3Homework1 {
+    public static void main(String[] args) {
+        int pi, radius, area, perimeter;
+
+        pi = 3;
+        radius = 5;
+        area = pi * radius * radius;
+        perimeter = 2 * pi * radius;
+
+        System.out.println("circle`s radius is " + radius);
+        System.out.println("circle`s area is " + area);
+        System.out.println("circle`s perimeter is " + perimeter);
+
+    }
+}
