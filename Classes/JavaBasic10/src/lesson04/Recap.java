30afd71f754b51b093cd5cb8eae8e779ea51436e
Ahmet <yasir.aksoy@outlook.com>
2025-09-17 03:15:51 +0300

    Recap

---
+++ Classes/JavaBasic10/src/lesson04/Recap.java
@@ -0,0 +1,39 @@
+package lesson04;
+
+public class Recap {
+    public static void main(String[] args) {
+
+        int num1 = 5 + 5 + 5;
+        System.out.println(num1);
+
+        int num2 = 5 + 5 * 5;
+        System.out.println(num2);
+
+
+        int n1 = 15;
+        int n2 = 2;
+
+        int div = n1 / n2;
+        System.out.println("div --> " + div);
+
+
+        int a = 12;
+        int b = 34;
+
+        String x = "Java";
+        String y = "Tuesday";
+
+        System.out.println(x + y + a);
+
+        System.out.println(x + a + b);
+
+        System.out.println(x + (a + b));
+
+        System.out.println("" + a + b);
+
+        System.out.println(x + y + a * b);
+
+        System.out.println();
+
+    }
+}
