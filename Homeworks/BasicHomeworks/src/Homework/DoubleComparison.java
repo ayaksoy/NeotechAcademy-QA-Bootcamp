0374e066add9dac0822e761554b5ce7a1de3b5e3
Ahmet <yasir.aksoy@outlook.com>
2025-09-17 16:34:25 +0300

    DoubleComparison

---
+++ Homeworks/BasicHomeworks/src/Homework/DoubleComparison.java
@@ -0,0 +1,15 @@
+package Homework;
+
+public class DoubleComparison {
+    public static void main(String[] args) {
+
+        double d1 = 111;
+        double d2 = 62;
+
+        if (d1 > d2)
+            System.out.println("Double value " + d1 + " is larger than " + d2 + ".");
+        else
+            System.out.println("Double value " + d1 + " is lower than " + d2 + ".");
+
+    }
+}
