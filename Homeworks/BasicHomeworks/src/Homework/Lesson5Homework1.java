74d9888709c62a3d7c984529f003315c722bbee4
Ahmet <yasir.aksoy@outlook.com>
2025-09-20 03:18:26 +0300

    check if a patient has allergies

---
+++ Homeworks/BasicHomeworks/src/Homework/Lesson5Homework1.java
@@ -0,0 +1,24 @@
+package Homework;
+
+public class Lesson5Homework1 {
+    public static void main(String[] args) {
+
+        boolean allergies = true;
+        short allergiesType = 6;
+
+        if (!allergies)
+            System.out.println("You’re healthy!");
+        else {
+            System.out.print("You have allergies ");
+            if (allergiesType == 1)
+                System.out.println("Don’t eat Peanut");
+            else if (allergiesType == 2)
+                System.out.println("Dont eat lactose");
+            else if (allergiesType == 3)
+                System.out.println("Dont eat seafood");
+            else
+                System.out.println("We dont kn0o9w what just be careful!");
+        }
+
+    }
+}
