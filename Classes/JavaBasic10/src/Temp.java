f637cbf0ddc204c35e37134d16e26922ed985c14
Ahmet <yasir.aksoy@outlook.com>
2025-10-10 02:22:20 +0300

    bugfixes

---
+++ Classes/JavaBasic10/src/Temp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char start = scan.next().charAt(0);
+        char end = scan.next().charAt(0);
+        // Write your code here
+
+        while (start <= end)
+            System.out.print(start++);
+
+    }
+}
--- Classes/JavaBasic10/src/lesson10/Hw4.java
+++ Classes/JavaBasic10/src/lesson10/Hw4.java
@@ -2,6 +2,5 @@ package lesson10;
 
 public class Hw4 {
     public static void main(String[] args) {
-        for (int i = 5)
     }
 }
