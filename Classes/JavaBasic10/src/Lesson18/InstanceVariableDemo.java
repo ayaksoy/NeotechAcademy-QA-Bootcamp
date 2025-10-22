4f135128f7bc8d8d9c42159bb933290ba9edc05b
Ahmet <yasir.aksoy@outlook.com>
2025-10-22 07:01:27 +0300

    lesson18

--- Classes/JavaBasic10/src/Lesson18/InstanceVariableDemo.java
+++ Classes/JavaBasic10/src/Lesson18/InstanceVariableDemo.java
@@ -11,11 +11,11 @@ public class InstanceVariableDemo {
         InstanceVariables iv3 = new InstanceVariables();
         System.out.println(iv3.name); // Faizan
         iv3.name = "Shekinah";
-
         System.out.println("--------------------");
         System.out.println(iv.name);   // Faizan
         System.out.println(iv2.name);  // Alisan
         System.out.println(iv3.name);  // shekinah
+        System.out.println(InstanceVariables.istrue);
 
     }
 }
--- Classes/JavaBasic10/src/Lesson18/InstanceVariables.java
+++ Classes/JavaBasic10/src/Lesson18/InstanceVariables.java
@@ -1,5 +1,6 @@
 package Lesson18;
 
 public class InstanceVariables {
+    static boolean istrue;
     String name = "Faizan";
 }
