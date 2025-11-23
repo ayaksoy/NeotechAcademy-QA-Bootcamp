6f53b16da2c25826a558b592ed8a23f6458d6259
Ahmet <yasir.aksoy@outlook.com>
2025-11-23 22:14:51 +0300

    Map task

---
+++ Classes/JavaBasic10/src/lesson30/Task1.java
@@ -0,0 +1,25 @@
+package lesson30;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Map<Integer, String> building = new HashMap<>();
+
+        building.put(1, "Google");
+        building.put(2, "Apple");
+        building.put(3, "NeoTech");
+        building.put(4, "Abc");
+        building.put(5, "Cnn");
+        building.put(6, "Google");
+        building.put(null, "OpenAi");
+
+        System.out.println(building);
+        System.out.println("Total Entry = " + building.size());
+
+        building.replace(4, "BCA");
+        System.out.println(building);
+
+    }
+}
