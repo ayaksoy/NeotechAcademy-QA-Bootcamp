1f1ba8f4cdfec3ecf8c606d0c6b60de379177189
Ahmet <yasir.aksoy@outlook.com>
2025-11-27 21:46:14 +0300

    lesson31 task

---
+++ Classes/JavaBasic10/src/lesson31/Task1.java
@@ -0,0 +1,35 @@
+package lesson31;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Map<String, Integer> cities = new TreeMap<>();
+
+        cities.put("Paris", 5);
+        cities.put("Moscowww", 6);
+        cities.put("Washington DC", 13);
+        cities.put("Ankara", 5);
+        cities.put("Erbil", 7);
+
+//        Set<Map.Entry<String, Integer>> setCities = cities.entrySet();
+//
+//        for (Map.Entry<String,Integer>city : setCities)
+//            if(city.getKey().length() > 7)
+//        use iterator cuz of remove;
+
+        Iterator<Entry<String, Integer>> it = cities.entrySet().iterator();
+
+        while (it.hasNext()) {
+            Entry<String, Integer> entry = it.next();
+
+            if (entry.getKey().length() > 7)
+                it.remove();
+        }
+        System.out.println(cities);
+
+    }
+}
