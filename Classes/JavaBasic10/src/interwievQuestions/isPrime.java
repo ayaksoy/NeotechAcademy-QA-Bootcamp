50d0e31ed350f72b3d8783d1c33defb3300698ce
Ahmet <yasir.aksoy@outlook.com>
2025-12-01 03:40:58 +0300

    iq isPrime

---
+++ Classes/JavaBasic10/src/interwievQuestions/isPrime.java
@@ -0,0 +1,16 @@
+package interwievQuestions;
+
+public class isPrime {
+    public static void main(String[] args) {
+        int a = 11;
+        boolean isAsal = true;
+        for (int i = a - 1; i > 1; i--) {
+            if (a % i == 0)
+                isAsal = false;
+        }
+        if (isAsal)
+            System.out.println("Sayi asal");
+        else
+            System.out.println("Sayi asal degil");
+    }
+}
