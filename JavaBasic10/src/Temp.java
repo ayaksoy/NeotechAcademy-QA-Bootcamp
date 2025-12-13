42d3efb26caf7433848fea64ebef4c9c066057c3
Ahmet <yasir.aksoy@outlook.com>
2025-12-13 09:57:14 +0300

    lesson2 homework1

--- JavaBasic10/src/Temp.java
+++ JavaBasic10/src/Temp.java
@@ -1,27 +1,8 @@
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Temp {
-    public static ArrayList<String> swap(ArrayList<String> list, int pos1, int pos2) {
-        // complete the method
-        String temp = list.get(pos1);
-        list.set(pos1, list.get(pos2));
-        list.set(pos2, temp);
-
-        return list;
-    }
-
-    // Do not touch below
     public static void main(String[] args) {
-        Map<String, Integer> map = new LinkedHashMap<>();
-        map.put("A", 1);
-        map.put("B", 2);
-        map.put("C", 3);
-        int sum = 0;
-        for (Integer key : map.values()) {
-            sum += key;
-        }
-        System.out.println(sum);
+        A deneme = new B();
+        B deneme2 = new A();
+
+        deneme2.bark();
     }
 }
---
+++ SeleniumBasic/src/test/java/com/neotech/lesson02/homeWork1.java
@@ -0,0 +1,29 @@
+package com.neotech.lesson02;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class homeWork1 {
+    public static void main(String[] args) {
+
+        WebDriver chrome = new ChromeDriver();
+
+        chrome.get("https://demo.guru99.com/test/newtours/");
+        chrome.findElement(By.linkText("REGISTER")).click();
+        chrome.findElement(By.name("firstName")).sendKeys("Ahmet");
+        chrome.findElement(By.name("lastName")).sendKeys("Aksoy");
+        chrome.findElement(By.name("phone")).sendKeys("5555555");
+        chrome.findElement(By.name("userName")).sendKeys("Ahmet@gamil.com");
+        chrome.findElement(By.name("address1")).sendKeys("istanbul/arn");
+        chrome.findElement(By.name("city")).sendKeys("istanbul");
+        chrome.findElement(By.name("state")).sendKeys("Turkey");
+        chrome.findElement(By.name("postalCode")).sendKeys("Ahmet");
+        chrome.findElement(By.name("country")).sendKeys("Ahmet");
+        chrome.findElement(By.name("email")).sendKeys("Ahmet");
+        chrome.findElement(By.name("password")).sendKeys("Ahmet");
+        chrome.findElement(By.name("confirmPassword")).sendKeys("Ahmet");
+        chrome.findElement(By.name("submit")).click();
+
+    }
+}
