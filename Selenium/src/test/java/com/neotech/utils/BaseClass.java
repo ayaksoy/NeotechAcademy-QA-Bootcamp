d155fdb18962c812a981f5f55164e209c5b46920
Ahmet <yasir.aksoy@outlook.com>
2025-12-20 21:50:59 +0300

    baseClass configured

--- Selenium/src/test/java/com/neotech/utils/BaseClass.java
+++ Selenium/src/test/java/com/neotech/utils/BaseClass.java
@@ -30,6 +30,27 @@ public class BaseClass {
 		String url = ConfigsReader.getProperty("url");
 		driver.get(url);
 	}
+    public static void setUp(String url) {
+        ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        String browser = ConfigsReader.getProperty("browser");
+
+        driver = null;
+        switch (browser.toLowerCase()) {
+            case "chrome": {
+                driver = new ChromeDriver();
+                break;
+            }
+            case "firefox": {
+                driver = new FirefoxDriver();
+                break;
+            }
+            default:
+                break;
+        }
+
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
 
 	public static void tearDown() {
 		if (driver != null) {
