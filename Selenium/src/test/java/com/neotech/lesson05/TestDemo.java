c98181fd111c6a6888b34b1a9924e1e000b5059d
Ahmet <yasir.aksoy@outlook.com>
2025-12-20 13:50:58 +0300

    lesson05 nad utils

---
+++ Selenium/src/test/java/com/neotech/lesson05/TestDemo.java
@@ -0,0 +1,16 @@
+package com.neotech.lesson05;
+
+import com.neotech.utils.BaseClass;
+
+public class TestDemo extends BaseClass {
+
+	public static void main(String[] args) {
+		setUp();
+
+		// do some testing here
+
+		tearDown();
+
+	}
+
+}
---
+++ Selenium/src/test/java/com/neotech/utils/BaseClass.java
@@ -0,0 +1,39 @@
+package com.neotech.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+
+	public static void setUp() {
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+		String browser = ConfigsReader.getProperty("browser");
+
+		driver = null;
+		switch (browser.toLowerCase()) {
+		case "chrome": {
+			driver = new ChromeDriver();
+			break;
+		}
+		case "firefox": {
+			driver = new FirefoxDriver();
+			break;
+		}
+		default:
+			break;
+		}
+
+		driver.manage().window().maximize();
+		String url = ConfigsReader.getProperty("url");
+		driver.get(url);
+	}
+
+	public static void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
